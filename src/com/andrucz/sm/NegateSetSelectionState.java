7ef3dea6b5ab6ba56282b864d9ef666b9d1a88d8
Acacio <andrucz@gmail.com>
2013-01-04 10:47:44 -0200

    hard work.

---
+++ src/com/andrucz/sm/NegateSetSelectionState.java
@@ -0,0 +1,46 @@
+package com.andrucz.sm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+final class NegateSetSelectionState<E> implements SelectionState<E> {
+
+	private final Set<E> elements;
+	
+	NegateSetSelectionState(E element) {
+		elements = new HashSet<E>();
+		elements.add(element);
+	}
+	
+	NegateSetSelectionState(Set<E> elements) {
+		this.elements = elements;
+	}
+	
+	@Override
+	public boolean isSelected(E element) {
+		return !elements.contains(element);
+	}
+
+	@Override
+	public SelectionState<E> select(E element) {
+		elements.remove(element);
+		
+		if (elements.isEmpty()) {
+			return new FullSelectionState<E>();
+		}
+		
+		return this;
+	}
+
+	@Override
+	public SelectionState<E> deselect(E element) {
+		elements.add(element);
+		return this;
+	}
+
+	@Override
+	public SelectionState<E> invert() {
+		return new SetSelectionState<E>(elements);
+	}
+
+}
